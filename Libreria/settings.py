773c6fcdbbaa0b4fd4b12351b06b0abb1e903141
Ranga <benjamingomez899@gmail.com>
2025-07-02 23:21:42 -0400

    ultimos cambios

--- Libreria/settings.py
+++ Libreria/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -113,6 +114,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Configuración de login
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
